f6dbf33584b5f39af4667d503f94904a132804a9
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-12 15:21:25 +0530

    Add files via upload

---
+++ addingatindexinarraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class addingatindexinarraylist{
+    public static void main(String args[]){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(345);
+        a.add(909);
+        a.add(767);
+        a.add(1,789);
+        System.out.println(a);
+    }
+}
