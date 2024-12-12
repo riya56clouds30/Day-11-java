9991c701e9e8f521a4b7400449e01657ccb9730f
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-12 15:22:11 +0530

    Add files via upload

---
+++ markerinterface.java
@@ -0,0 +1,16 @@
+import java.util.*;
+interface printable{
+}
+class h implements printable{
+    public void print()
+    {
+        System.out.println("hello");}
+}
+public class markerinterface{
+    public static void main(String[]args)
+    {
+        h f=new h();
+        f.print();
+            
+    }
+}
