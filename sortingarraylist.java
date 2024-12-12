1946cba73f479a46fc849a2755889c36dd9976c6
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-12 15:23:10 +0530

    Add files via upload

---
+++ sortingarraylist.java
@@ -0,0 +1,13 @@
+ import java.util.*;
+ public class sortingarraylist{
+    public static void main(String args[]){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(45);
+        a.add(1);
+        a.add(18);
+        a.add(12);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+ }
+ 
