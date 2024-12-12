8a60c8a879ccccd76970bac447f15041f889840f
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-12 15:22:26 +0530

    Add files via upload

---
+++ modifyingarraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class modifyingarraylist{
+    public static void main(String args[]){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(345);
+        a.add(909);
+        a.add(767);
+        a.set(2,7898);
+        System.out.println(a);
+    }
+}
