c2c4cec97bc88e44a8e2d114af1d3ef89851ed18
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-13 12:36:47 +0530

    Add files via upload

---
+++ acceptinglong.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class acceptinglong{
+    public static void main(String args[])
+    {
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
