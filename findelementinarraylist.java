d404b479e4a3d74b620e2ec00b6b00d7aa9f43c5
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-13 12:36:28 +0530

    Add files via upload

---
+++ findelementinarraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class findelementinarraylist {
+    public static void main(String args[]){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(90);
+        a.add(89);
+        a.add(767);
+        System.out.println(a.contains(90));
+        System.out.println(a.contains(900));
+    }
+}
