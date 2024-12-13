3e4bcd1f7f163d3d9dc7984c1a055f48983275c2
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-13 12:37:03 +0530

    Add files via upload

---
+++ allotingsizeandfindingarraysize.java
@@ -0,0 +1,12 @@
+import java .util.*;
+public class allotingsizeandfindingarraysize{
+    public static void main(String args[])
+    {
+        ArrayList<Integer>a=new ArrayList<>(3);
+        a.add(898);
+        a.add(89);
+        a.add(1);
+        a.add(78);
+        System.out.println(a.size());
+    }
+}
